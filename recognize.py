ddb1226807f144ffde6e83b7688177193052ee52
realchrispbacon <chrispunt13@icloud.com>
2020-04-21 15:56:33 -0400

    transmitting image ove mqtt

--- recognize.py
+++ recognize.py
@@ -12,10 +12,18 @@ import pickle
 import cv2
 import os
 
+import paho.mqtt.client as mqtt
+import time
+
+
+BROKER = '97.95.108.173'
+PORT = 1883
+QOS = 1
+
+
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="path to input image")
+
 ap.add_argument("-d", "--detector", required=True,
 	help="path to OpenCV's deep learning face detector")
 ap.add_argument("-m", "--embedding-model", required=True,
@@ -28,81 +36,119 @@ ap.add_argument("-c", "--confidence", type=float, default=0.5,
 	help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
 
-# load our serialized face detector from disk
-print("[INFO] loading face detector...")
-protoPath = os.path.sep.join([args["detector"], "deploy.prototxt"])
-modelPath = os.path.sep.join([args["detector"],
-	"res10_300x300_ssd_iter_140000.caffemodel"])
-detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
 
-# load our serialized face embedding model from disk
-print("[INFO] loading face recognizer...")
-embedder = cv2.dnn.readNetFromTorch(args["embedding_model"])
+def recognizeFaces(img):
+    # load our serialized face detector from disk
+    print("[INFO] loading face detector...")
+    protoPath = os.path.sep.join([args["detector"], "deploy.prototxt"])
+    modelPath = os.path.sep.join([args["detector"],
+        "res10_300x300_ssd_iter_140000.caffemodel"])
+    detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
 
-# load the actual face recognition model along with the label encoder
-recognizer = pickle.loads(open(args["recognizer"], "rb").read())
-le = pickle.loads(open(args["le"], "rb").read())
+    # load our serialized face embedding model from disk
+    print("[INFO] loading face recognizer...")
+    embedder = cv2.dnn.readNetFromTorch(args["embedding_model"])
 
-# load the image, resize it to have a width of 600 pixels (while
-# maintaining the aspect ratio), and then grab the image dimensions
-image = cv2.imread(args["image"])
-image = imutils.resize(image, width=600)
-(h, w) = image.shape[:2]
+    # load the actual face recognition model along with the label encoder
+    recognizer = pickle.loads(open(args["recognizer"], "rb").read())
+    le = pickle.loads(open(args["le"], "rb").read())
 
-# construct a blob from the image
-imageBlob = cv2.dnn.blobFromImage(
-	cv2.resize(image, (300, 300)), 1.0, (300, 300),
-	(104.0, 177.0, 123.0), swapRB=False, crop=False)
+    # load the image, resize it to have a width of 600 pixels (while
+    # maintaining the aspect ratio), and then grab the image dimensions
+    image = cv2.imread(img)
+    image = imutils.resize(image, width=600)
+    (h, w) = image.shape[:2]
+    print("image was loaded")
 
-# apply OpenCV's deep learning-based face detector to localize
-# faces in the input image
-detector.setInput(imageBlob)
-detections = detector.forward()
+    # construct a blob from the image
+    imageBlob = cv2.dnn.blobFromImage(
+        cv2.resize(image, (300, 300)), 1.0, (300, 300),
+        (104.0, 177.0, 123.0), swapRB=False, crop=False)
 
-# loop over the detections
-for i in range(0, detections.shape[2]):
-	# extract the confidence (i.e., probability) associated with the
-	# prediction
-	confidence = detections[0, 0, i, 2]
+    # apply OpenCV's deep learning-based face detector to localize
+    # faces in the input image
+    detector.setInput(imageBlob)
+    detections = detector.forward()
 
-	# filter out weak detections
-	if confidence > args["confidence"]:
-		# compute the (x, y)-coordinates of the bounding box for the
-		# face
-		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-		(startX, startY, endX, endY) = box.astype("int")
+    # loop over the detections
+    for i in range(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with the
+        # prediction
+        confidence = detections[0, 0, i, 2]
 
-		# extract the face ROI
-		face = image[startY:endY, startX:endX]
-		(fH, fW) = face.shape[:2]
+        # filter out weak detections
+        if confidence > args["confidence"]:
+            # compute the (x, y)-coordinates of the bounding box for the
+            # face
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
 
-		# ensure the face width and height are sufficiently large
-		if fW < 20 or fH < 20:
-			continue
+            # extract the face ROI
+            face = image[startY:endY, startX:endX]
+            (fH, fW) = face.shape[:2]
 
-		# construct a blob for the face ROI, then pass the blob
-		# through our face embedding model to obtain the 128-d
-		# quantification of the face
-		faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255, (96, 96),
-			(0, 0, 0), swapRB=True, crop=False)
-		embedder.setInput(faceBlob)
-		vec = embedder.forward()
+            # ensure the face width and height are sufficiently large
+            if fW < 20 or fH < 20:
+                continue
 
-		# perform classification to recognize the face
-		preds = recognizer.predict_proba(vec)[0]
-		j = np.argmax(preds)
-		proba = preds[j]
-		name = le.classes_[j]
+            # construct a blob for the face ROI, then pass the blob
+            # through our face embedding model to obtain the 128-d
+            # quantification of the face
+            faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255, (96, 96),
+                (0, 0, 0), swapRB=True, crop=False)
+            embedder.setInput(faceBlob)
+            vec = embedder.forward()
 
-		# draw the bounding box of the face along with the associated
-		# probability
-		text = "{}: {:.2f}%".format(name, proba * 100)
-		y = startY - 10 if startY - 10 > 10 else startY + 10
-		cv2.rectangle(image, (startX, startY), (endX, endY),
-			(0, 0, 255), 2)
-		cv2.putText(image, text, (startX, y),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+            # perform classification to recognize the face
+            preds = recognizer.predict_proba(vec)[0]
+            j = np.argmax(preds)
+            proba = preds[j]
+            name = le.classes_[j]
 
-# show the output image
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+            # draw the bounding box of the face along with the associated
+            # probability
+            text = "{}: {:.2f}%".format(name, proba * 100)
+            y = startY - 10 if startY - 10 > 10 else startY + 10
+            cv2.rectangle(image, (startX, startY), (endX, endY),
+                (0, 0, 255), 2)
+            cv2.putText(image, text, (startX, y),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+    # show the output image
+    # cv2.imshow("Image", image)
+    print("writing the image to file")
+    cv2.imwrite("output1.jpg", image)
+    # cv2.waitKey(0)
+
+
+# Callback when a connection has been established with the MQTT broker
+def on_connect(client, userdata, rc, *extra_params):
+    print('Connected with result code='+str(rc))
+
+# Callback when client receives a message from the broker
+# Use button message to turn LED on/off
+def on_message(client, data, msg):
+    if msg.topic == "chrisNate/Image":
+        print("message received")
+
+        #image received
+        with open("output.jpg", "wb") as f:
+            f.write(msg.payload)
+
+        recognizeFaces("output.jpg")
+
+
+# Setup MQTT client and callbacks
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+# Connect to MQTT broker and subscribe to the button topic
+client.connect(BROKER, PORT, 60)
+client.subscribe("chrisNate/Image", qos=QOS)
+client.loop_start()
+client.loop_start()
+while True:
+    time.sleep(10)
+print("Done")
+client.disconnect()
